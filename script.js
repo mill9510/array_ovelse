c61b7b19705a08d258d2f3a095fd072e2c31aabb
Mill9510 <mill9510@stud.kea.dk>
2023-02-02 09:41:40 +0100

    playliste

---
+++ script.js
@@ -0,0 +1,8 @@
+const playList = ["Who wants to live forever", "Sthlm Sunset", "The Last Goodbye", "Woo", "Paint it Black", "Come and Get Your Love", "Never Tear Us Apart", "Forever Young", "Glue", "Redbone"];
+
+playList.forEach(visEnAfGangen);
+
+function visEnAfGangen(playList) {
+  console.log(playList + " er god");
+  document.querySelector("#playList").innerHTML += `<p>${playList}</p>`;
+}
